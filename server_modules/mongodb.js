d2465a1c7f2e59fa7ce5623951c55b07b1ae9bbb
Williamhiller <1747768963@qq.com>
2019-10-27 22:31:56 +0800

    ids 接口

--- server_modules/mongodb.js
+++ server_modules/mongodb.js
@@ -1,8 +1,22 @@
 //   引入
 const mongoose = require('mongoose');
-const config = require('../config/config');
+// const config = require('../config/config');
 // 用于异步回调
 mongoose.Promise = require('bluebird');
-global.db = mongoose.connect(`mongodb://${config.db.servername}:${config.db.port}/${config.db.DATABASE}`);
+const uri = "mongodb://william:liu123@cluster0-jyw35.mongodb.net/test?retryWrites=true&w=majority";
+// global.db = mongoose.connect(`mongodb://${config.db.servername}:${config.db.port}/${config.db.DATABASE}`);
+global.db = mongoose.connect(uri, { useMongoClient: true });
 
 module.exports = mongoose
+
+
+// const MongoClient = require('mongodb').MongoClient;
+// const uri = "mongodb+srv://william:liu123@cluster0-jyw35.mongodb.net/test?retryWrites=true&w=majority";
+// const client = new MongoClient(uri, { useUnifiedTopology: true });
+// client.connect(err => {
+//   const collection = client.db("vuechat").collection("test");
+//   console.log(collection)
+//   // perform actions on the collection object
+//   client.close();
+// });
+
